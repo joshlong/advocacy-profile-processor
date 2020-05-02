fff9dc4ce253803a2684fe3589c83df7329ac4ef
Josh Long <josh@joshlong.com>
2020-05-01 23:06:38 -0700

    up

--- run.sh
+++ run.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-ROOT_DIR=`pwd`
+ROOT_DIR=$(pwd)
 git config --global user.email "josh@joshlong.com"
 git config --global user.name "Advocacy Profile Processor"
 
 OUTPUT_DIR=$HOME/Desktop/out
 CLONE_DIR=$OUTPUT_DIR/clone
+JTT="$CLONE_DIR/jtt"
+VTT="$CLONE_DIR/vtt"
 export PROFILE_PAGE=$CLONE_DIR/content/advocate/joshlong.md
 
 ts=$(date +%s)
@@ -17,21 +19,21 @@ rm -rf $OUTPUT_DIR && mkdir -p $OUTPUT_DIR
 
 # here's what I need to do:
 # - git clone the actual repo
-git clone https://${GIT_USERNAME}:${GIT_PASSWORD}@github.com/vmware-tanzu/tanzu-tuesdays.git "$CLONE_DIR/vtt"
-git clone https://${GIT_USERNAME}:${GIT_PASSWORD}@github.com/joshlong/tanzu-tuesdays.git "$CLONE_DIR/jtt"
+git clone https://${GIT_USERNAME}:${GIT_PASSWORD}@github.com/vmware-tanzu/tanzu-tuesdays.git $VTT
+git clone https://${GIT_USERNAME}:${GIT_PASSWORD}@github.com/joshlong/tanzu-tuesdays.git $JTT
 
 # - move the config for my repo into the actual repo
 mv "$CLONE_DIR"/jtt/.git/config "$CLONE_DIR"/vtt/.git/config
 
-cd "$CLONE_DIR"/vtt/
+cd $VTT
 
 # - then force push the actual repo into my repo (no need to worry about merging stuff if, for sure, im using the latest version of the repo!)
 git push --force
 
 # - then clone my repo and do the change on it
 rm -rf "$CLONE_DIR/jtt" && rm -rf "$CLONE_DIR/vtt"
-git clone https://${GIT_USERNAME}:${GIT_PASSWORD}@github.com/joshlong/tanzu-tuesdays.git "$CLONE_DIR/jtt"
-cd "$CLONE_DIR"/jtt/
+git clone https://${GIT_USERNAME}:${GIT_PASSWORD}@github.com/joshlong/tanzu-tuesdays.git $JTT
+cd $JTT
 
 hub pull-request -b vmware-tanzu/tanzu-tuesdays:master -m $message || echo "there's already a PR, so just updating it"
 
@@ -41,7 +43,7 @@ cd $ROOT_DIR
 pipenv install
 pipenv run python main.py
 
-cd "$CLONE_DIR"/jtt
+cd $JTT
 git add "$PROFILE_PAGE"
 git commit -a -m "${message}"
 git push
