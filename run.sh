c735a60437eefa25675369b948034efea01b6ab4
Josh Long <josh@joshlong.com>
2020-05-01 23:17:03 -0700

    run

--- run.sh
+++ run.sh
@@ -8,10 +8,13 @@ OUTPUT_DIR=$HOME/Desktop/out
 CLONE_DIR=$OUTPUT_DIR/clone
 JTT="$CLONE_DIR/jtt"
 VTT="$CLONE_DIR/vtt"
+
+mkdir -p $CLONE_DIR
+
 export PROFILE_PAGE=$CLONE_DIR/content/advocate/joshlong.md
 
-ts=$(date +%s)
-message="update joshlong.md @ ${ts}"
+TS=$(date +%s)
+MESSAGE="update joshlong.md @ ${ts}"
 
 rm -rf $OUTPUT_DIR && mkdir -p $OUTPUT_DIR
 
@@ -19,23 +22,23 @@ rm -rf $OUTPUT_DIR && mkdir -p $OUTPUT_DIR
 
 # here's what I need to do:
 # - git clone the actual repo
-git clone https://${GIT_USERNAME}:${GIT_PASSWORD}@github.com/vmware-tanzu/tanzu-tuesdays.git $VTT
+git clone https://${GIT_USERNAME}:${GIT_PASSWORD}@github.com/vmware-tanzu/tanzu-tuesdays.git $VTT || die "couldn't clone "
 git clone https://${GIT_USERNAME}:${GIT_PASSWORD}@github.com/joshlong/tanzu-tuesdays.git $JTT
 
 # - move the config for my repo into the actual repo
-mv "$CLONE_DIR"/jtt/.git/config "$CLONE_DIR"/vtt/.git/config
+mv $JTT/.git/config $VTT/.git/config
 
-cd $VTT
+cd $VTT || die "couldn't go to the $VTT directory."
 
 # - then force push the actual repo into my repo (no need to worry about merging stuff if, for sure, im using the latest version of the repo!)
 git push --force
 
 # - then clone my repo and do the change on it
-rm -rf "$CLONE_DIR/jtt" && rm -rf "$CLONE_DIR/vtt"
+rm -rf  $JTT && rm -rf $VTT
 git clone https://${GIT_USERNAME}:${GIT_PASSWORD}@github.com/joshlong/tanzu-tuesdays.git $JTT
 cd $JTT
 
-hub pull-request -b vmware-tanzu/tanzu-tuesdays:master -m $message || echo "there's already a PR, so just updating it"
+hub pull-request -b vmware-tanzu/tanzu-tuesdays:master -m $MESSAGE || echo "there's already a PR, so just updating it"
 
 #
 cd $ROOT_DIR
@@ -45,5 +48,5 @@ pipenv run python main.py
 
 cd $JTT
 git add "$PROFILE_PAGE"
-git commit -a -m "${message}"
+git commit -a -m "${MESSAGE}"
 git push
